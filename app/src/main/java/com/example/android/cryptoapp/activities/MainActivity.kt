dce026d9baaa0a812607ccc321372f6f0f853654
FAVOUR <truelyfavoured@gmail.com>
2020-09-01 23:27:07 +0100

    Created a MainActivity.kt class

---
+++ app/src/main/java/com/example/android/cryptoapp/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android.cryptoapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.cryptoapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
