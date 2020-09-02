47a89e459ffda31e873129643a4423bcc6f30749
FAVOUR <truelyfavoured@gmail.com>
2020-09-02 01:54:22 +0100

    Successfully converted List Activity to ListFragment.kt

--- app/src/main/java/com/example/android/cryptoapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/android/cryptoapp/activities/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.android.cryptoapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.android.cryptoapp.R
+import com.example.android.cryptoapp.fragments.ListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var listFragment = ListFragment()
+
+                      supportFragmentManager.beginTransaction()
+                                            .add(listFragment,null)
+                                            .commit()
 
     }
 }
