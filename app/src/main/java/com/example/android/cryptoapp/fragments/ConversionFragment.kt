04f8b1a32605e9dc2ef99ae5226d098f4b6340cb
FAVOUR <truelyfavoured@gmail.com>
2020-09-04 20:49:51 +0100

    Implemented the ConversionFragment.kt

--- app/src/main/java/com/example/android/cryptoapp/fragments/ConversionFragment.kt
+++ app/src/main/java/com/example/android/cryptoapp/fragments/ConversionFragment.kt
@@ -1,11 +1,23 @@
 package com.example.android.cryptoapp.fragments
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import com.example.android.cryptoapp.R
+import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.CropCircleTransformation
+import kotlinx.android.synthetic.main.fragment_conversion.*
+import java.text.DecimalFormat
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +34,30 @@ class ConversionFragment : Fragment() {
 //    private var param1: String? = null
 //    private var param2: String? = null
 
+    var btc_amount: EditText? = null
+    var eth_amount: EditText? = null
+    var currencyImage: ImageView? = null
+    var btcImage: ImageView? = null
+    var ethImage: ImageView? = null
+    var currency_amount: TextView? = null
+    var btcExchangeRate: TextView? = null
+    var ethExchangeRate: TextView? = null
+    var currencySymbol_1: TextView? = null
+    var currencySymbol_2: TextView? = null
+    var currency_Name: TextView? = null
+    var currencyAbbreviation: TextView? = null
+    var image = 0
+    var check = false
+    var currencyAbr: String? = null
+    var currencySymbol: String? = null
+    var currencyName: String? = null
+    var btcRate: Double? = null
+    var ethRate: Double? = null
+    var format: DecimalFormat? = null
+    var bundle: Bundle? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        arguments?.let {
@@ -34,6 +70,60 @@ class ConversionFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_conversion, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        bundle = arguments
+
+        check = bundle == null
+        btcExchangeRate = btc_exchangerate
+        ethExchangeRate = eth_exchangerate
+        currencySymbol_1 = currency_symbol
+        currencySymbol_2 = currency_symbol_2
+        currencyAbbreviation = abrivation
+        currency_Name = currency_name
+        format = DecimalFormat()
+        format!!.isGroupingUsed = true
+        format!!.maximumIntegerDigits = 20
+        format!!.maximumFractionDigits = 3
+        currency_amount = currency_amount
+        btc_amount = btc_amount
+        eth_amount =eth_amount
+        btc_amount!!.addTextChangedListener(generalWatcher)
+        eth_amount!!.addTextChangedListener(generalWatcher)
+        if (!check) {
+            image = bundle?.getInt("image") ?:0
+            btcRate = bundle?.getDouble("btcRate")
+            currencyAbr = bundle?.getString("currencyAbr")
+            currencySymbol = bundle?.getString("currencySymbol")
+            currencyName = bundle?.getString("currencyName")
+            ethRate =  bundle?.getDouble("ethRate")
+        }
+
+        //Set the currency Image
+        currencyImage = currency_image
+        Picasso.get().load(image)
+                .transform(CropCircleTransformation())
+                .into(currencyImage)
+
+        //Set the btc Image
+        btcImage = btc_logo
+        Picasso.get().load(R.drawable.btc)
+                .transform(CropCircleTransformation())
+                .into(btcImage)
+
+        //Set the eth Image
+        ethImage = eth_logo
+        Picasso.get().load(R.drawable.eth)
+                .transform(CropCircleTransformation())
+                .into(ethImage)
+        btcExchangeRate!!.text = format!!.format(btcRate!!)
+        ethExchangeRate!!.text = format!!.format(ethRate!!)
+        currencySymbol_1!!.text = currencySymbol
+        currencySymbol_2!!.text = currencySymbol
+        currencyAbbreviation!!.text = currencyAbr
+        currency_Name!!.text = currencyName
     }
 
 //    companion object {
@@ -55,4 +145,53 @@ class ConversionFragment : Fragment() {
 //                    }
 //                }
 //    }
+
+    private val generalWatcher: TextWatcher = object : TextWatcher {
+        var ignore = true //This works fine with java but with kotlin
+        override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+        override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+        override fun afterTextChanged(editable: Editable) {
+            ignore = true //You have to reassign with kotlin you should try this
+            try {
+                val fBtcValue: Float
+                val sBtcValue: String
+                if (btc_amount!!.text.hashCode() == editable.hashCode()) {
+                    if (editable.length > 0) {
+                        if (ignore) {
+                            eth_amount!!.setText("")
+                            fBtcValue = btc_amount!!.text.toString().trim { it <= ' ' }.toFloat()
+                            sBtcValue = format!!.format(btcRate!! * fBtcValue)
+                            currency_amount!!.setTextColor(Color.parseColor("#ab7d0b"))
+                            currency_amount!!.text = currencySymbol + sBtcValue
+                            return
+                        } else {
+                            currency_amount!!.text = ""
+                        }
+                        ignore = false
+                    }
+                } else if (eth_amount!!.text.hashCode() == editable.hashCode()) {
+                    val sEthValue: String
+                    if (editable.length > 0) {
+                        if (ignore) {
+                            btc_amount!!.setText("")
+                            val fEthValue = eth_amount!!.text.toString().trim { it <= ' ' }.toFloat()
+                            sEthValue = format!!.format(ethRate!! * fEthValue)
+                            currency_amount!!.setTextColor(Color.parseColor("#5b6abd"))
+                            currency_amount!!.text = currencySymbol + sEthValue
+                            return
+                        } else {
+                            currency_amount!!.text = ""
+                        }
+                        ignore = false
+                    }
+                }
+            } catch (e: NumberFormatException) {
+                btc_amount!!.setText("")
+                eth_amount!!.setText("")
+                currency_amount!!.text = ""
+                Toast.makeText(requireContext(), "You can only Enter Numbers!", Toast.LENGTH_LONG).show()
+            }
+        }
+    } //    //addCurrency button
+
 }
