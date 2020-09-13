748aaac0faa1bb7d8106e8dfb302d6a1dd20fb19
FAVOUR <truelyfavoured@gmail.com>
2020-09-13 23:25:46 +0100

    Setup the editor viewmodel

--- app/src/main/java/com/example/android/cryptoapp/viewmodel/EditorViewModel.kt
+++ app/src/main/java/com/example/android/cryptoapp/viewmodel/EditorViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.android.cryptoapp.viewmodel
 
-class EditorViewModel {
+import android.widget.Spinner
+import androidx.lifecycle.ViewModel
+import com.example.android.cryptoapp.currency_data.Btc
+import com.example.android.cryptoapp.currency_data.Eth
+import com.example.android.cryptoapp.currency_data.JsonResponse
+import com.example.android.cryptoapp.rest.CryptoCurrencyService
+
+class EditorViewModel :ViewModel() {
+    var currencySpinner: Spinner? = null
+    var cryptoClient: CryptoCurrencyService? = null
+    var btcConversionRates: Btc? = null
+    var ethConversionRates: Eth? = null
+    var conversionFromBtc: Double? = null
+    var conversionFromEth: Double? = null
+    var currencyAbr: String? = null
+    var currencySymbol: String? = null
+    var _currencyName: String? = null
+    var image = 0
+    var jsonResponse: JsonResponse? = null
+
+
+
 }
