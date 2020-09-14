2853f3d466563d55358e641ae0e211cc0041b835
FAVOUR <truelyfavoured@gmail.com>
2020-09-14 23:15:06 +0100

    Did some cleanup

--- app/src/main/java/com/example/android/cryptoapp/fragments/EditorFragment.kt
+++ app/src/main/java/com/example/android/cryptoapp/fragments/EditorFragment.kt
@@ -38,18 +38,7 @@ class EditorFragment : DialogFragment() {
 
 
     private lateinit var mOnDataGotten:OnDataGotten
-//    var loading: RelativeLayout? = null
-
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-     /*   arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }*/
-    }
+//    var loading: RelativeLayout? = nul
 
     // activity-ktx artifact
     private val viewmodel: EditorViewModel by viewModels()
