382f4e33ec9827ef7bf990a405dad1079e5371ea
FAVOUR <truelyfavoured@gmail.com>
2020-09-20 19:16:38 +0100

    Done with refactoring the ConversionFragment.kt and ConversionViewmodel.kt

--- app/src/main/java/com/example/android/cryptoapp/viewmodel/ConversionViewmodel.kt
+++ app/src/main/java/com/example/android/cryptoapp/viewmodel/ConversionViewmodel.kt
@@ -3,15 +3,16 @@ package com.example.android.cryptoapp.viewmodel
 import android.os.Bundle
 import androidx.lifecycle.ViewModel
 import java.text.DecimalFormat
+import kotlin.properties.Delegates
 
 class ConversionViewmodel:ViewModel() {
     var image = 0
     var check = false
-    var currencyAbr: String? = null
-    var currencySymbol: String? = null
-    var currencyName: String? = null
-    var btcRate: Double? = null
-    var ethRate: Double? = null
-    var format: DecimalFormat? = null
-    var bundle: Bundle? = null
+    lateinit var currencyAbr: String
+    lateinit var currencySymbol: String
+    lateinit var currencyName: String
+    var btcRate by Delegates.notNull<Double>()
+    var ethRate by Delegates.notNull<Double>()
+    lateinit var format: DecimalFormat
+    lateinit var bundle: Bundle
 }
