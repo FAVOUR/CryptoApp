cf0fe40bca381446273a85ef60de53a02cedaab8
FAVOUR <truelyfavoured@gmail.com>
2020-09-22 23:36:49 +0100

    Did a few cleanup for EditorFragment.kt

--- app/src/main/java/com/example/android/cryptoapp/fragments/EditorFragment.kt
+++ app/src/main/java/com/example/android/cryptoapp/fragments/EditorFragment.kt
@@ -26,10 +26,6 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -395,23 +391,5 @@ class EditorFragment : DialogFragment() {
         return ethRates
     }
 
-    /*companion object {
-        *//**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EditorFragment.
-         *//*
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                EditorFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }*/
+
 }
