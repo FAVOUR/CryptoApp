810549fb611a4aba27cdeb687219165ad4c5e128
FAVOUR <truelyfavoured@gmail.com>
2020-09-26 12:35:12 +0100

    Setup the database

--- app/src/main/java/com/example/android/cryptoapp/database/CurrencyDao.kt
+++ app/src/main/java/com/example/android/cryptoapp/database/CurrencyDao.kt
@@ -1,4 +1,18 @@
 package com.example.android.cryptoapp.database
 
-class CurrencyDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface CurrencyDao {
+
+    @Query("Select * from CryptoCurrencyData")
+   suspend fun  getSavedCurrencies(): List<CryptoCurrencyData>
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend  fun saveCurrency(cryptoCurrencyData: CryptoCurrencyData)
 }
--- app/src/main/java/com/example/android/cryptoapp/database/CurrencyRoomDatabase.kt
+++ app/src/main/java/com/example/android/cryptoapp/database/CurrencyRoomDatabase.kt
@@ -1,9 +1,11 @@
 package com.example.android.cryptoapp.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [CryptoCurrencyData::class],version = 1,exportSchema = false)
 abstract class CurrencyRoomDatabase : RoomDatabase() {
 
     abstract fun currencyDao() : CurrencyDao
