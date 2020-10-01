c181eedae5f1b90c00679db5f592268e9b3dee63
FAVOUR <truelyfavoured@gmail.com>
2020-10-01 23:51:02 +0100

    Modified the CryptoCurrencyRates.kt class

---
+++ app/src/main/java/com/example/android/cryptoapp/domain/model/CryptoCurrencyRates.kt
@@ -0,0 +1,18 @@
+package com.example.android.cryptoapp.domain.model
+
+import com.example.android.cryptoapp.data.source.local.db.CryptoCurrencyData
+
+
+//TODO
+ /*  1)change the parameter name of first and second rate
+   2) Rename the class probably currency crypto */
+class CryptoCurrencyRates(val image: Int, val firstExRate: Double, val secondExRate: Double, val name: String, val abbrivation: String, val symbol: String){
+
+    fun  List<CryptoCurrencyData>.asDomainModel():List<CryptoCurrencyRates>{
+
+        return map {
+            CryptoCurrencyRates(image = it.image,firstExRate =  it.btcRate,secondExRate =  it.ethRate, name = it.currencyName, abbrivation = it.currencyAbbreviation, symbol = it.currencySymbol)
+        }
+
+    }
+}
