83cfc7e3bc8645bc64141b3a0aead05833de8c9b
FAVOUR <truelyfavoured@gmail.com>
2020-10-03 22:56:58 +0100

    Updated the CurrencyDao.kt

--- app/src/main/java/com/example/android/cryptoapp/data/source/local/db/CurrencyDao.kt
+++ app/src/main/java/com/example/android/cryptoapp/data/source/local/db/CurrencyDao.kt
@@ -1,17 +1,29 @@
 package com.example.android.cryptoapp.data.source.local.db
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
 @Dao
 interface CurrencyDao {
 
     @Query("Select * from CryptoCurrencyData")
-   suspend fun  getSavedCurrencies(): List<CryptoCurrencyData>
+   suspend fun  getLocalCryptoRates(): List<CryptoCurrencyData>
 
+    @Query("Select * from CryptoCurrencyData")
+     fun ObserveCryptoRates():LiveData<List<CryptoCurrencyData>>
+
+    @Query("Select * from CryptoCurrencyData where id = :rateId")
+    fun getCryptoRateById(rateId:String):List<CryptoCurrencyData>
+
+    @Query("Delete from CryptoCurrencyData ")
+    fun deleteAllCryptoRates():List<CryptoCurrencyData>
+
+    @Delete
+    fun deleteCryptoRate(cryptoCurrencyData: CryptoCurrencyData)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend  fun saveCurrency(cryptoCurrencyData: CryptoCurrencyData)
+    suspend  fun saveCryptoCurrencyRate(cryptoCurrencyData: CryptoCurrencyData)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend  fun saveCryptoCurrencyRates(cryptoCurrencyData: List<CryptoCurrencyData>)
 }
