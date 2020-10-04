8c69810078164f910e0f82d59a62fa21495743e8
FAVOUR <truelyfavoured@gmail.com>
2020-10-04 01:15:14 +0100

    Added the interface method

--- app/src/main/java/com/example/android/cryptoapp/data/source/CommonCryptoRatesDataSource.kt
+++ app/src/main/java/com/example/android/cryptoapp/data/source/CommonCryptoRatesDataSource.kt
@@ -1,12 +1,12 @@
 package com.example.android.cryptoapp.data.source
 
 import com.example.android.cryptoapp.currency_data.CurrencyAbbreviation
+import com.example.android.cryptoapp.data.source.local.db.CryptoCurrencyData
 import com.example.android.cryptoapp.domain.model.CryptoCurrencyRates
 import com.example.android.cryptoapp.util.Result
 
 interface CommonCryptoRatesDataSource {
 
-    suspend fun getCryptoRates(): Result<List<CryptoCurrencyRates>>
-    suspend fun getCryptoRate(cryptoCurrencyAbbreviation: CurrencyAbbreviation): Result<CryptoCurrencyRates>
+    suspend fun getCryptoRates(cryptoCurrencyAbbreviation: String): Result<List<CryptoCurrencyData>>
 
 }
--- app/src/main/java/com/example/android/cryptoapp/data/source/ILocalCryptoRatesDataSource.kt
+++ app/src/main/java/com/example/android/cryptoapp/data/source/ILocalCryptoRatesDataSource.kt
@@ -1,17 +1,23 @@
 package com.example.android.cryptoapp.data.source
 
 import androidx.lifecycle.LiveData
+import com.example.android.cryptoapp.currency_data.CurrencyAbbreviation
 import com.example.android.cryptoapp.data.source.CommonCryptoRatesDataSource
+import com.example.android.cryptoapp.data.source.local.db.CryptoCurrencyData
 import com.example.android.cryptoapp.domain.model.CryptoCurrencyRates
 import com.example.android.cryptoapp.util.Result
 
 interface ILocalCryptoRatesDataSource : CommonCryptoRatesDataSource {
 
-    fun observeCryptoRates(): LiveData<Result<List<CryptoCurrencyRates>>>
+    fun observeCryptoRates(): LiveData<Result<List<CryptoCurrencyData>>>
 
-    suspend fun saveCryptoRate()
+    suspend fun getCryptoRateById(id: Int): Result<List<CryptoCurrencyData>>
 
-    suspend fun deleteCryptoRate(rateId:String)
+    suspend fun saveCryptoRate(cryptoRateData : List<CryptoCurrencyData>)
+
+    suspend fun saveCryptoRate(cryptoRateData : CryptoCurrencyData)
+
+    suspend fun deleteCryptoRate(cryptoCurrencyData: CryptoCurrencyData)
 
     suspend fun deleteAllCurrencyRate()
 
