c4a064b5297b8b58823e529f6369792bb090788b
FAVOUR <truelyfavoured@gmail.com>
2020-10-09 03:29:02 +0100

    Created the app component class

--- app/src/main/java/com/example/android/cryptoapp/di/AppComponent.kt
+++ app/src/main/java/com/example/android/cryptoapp/di/AppComponent.kt
@@ -1,2 +1,12 @@
 package com.example.android.cryptoapp.di
 
+import dagger.Component
+import dagger.Module
+
+
+@Component(modules = [AppModule::class])
+class AppComponent {
+
+
+}
+
--- app/src/main/java/com/example/android/cryptoapp/di/AppModule.kt
+++ app/src/main/java/com/example/android/cryptoapp/di/AppModule.kt
@@ -1,10 +1,69 @@
 package com.example.android.cryptoapp.di
 
+import com.example.android.cryptoapp.util.Constants
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.squareup.picasso.Picasso
+import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
- object AppModule {
+object AppModule {
 
+
+    //Provide an instance of Picasso
+    @Singleton //This indicates that it is tied to the livecycleScope
+    @Provides
     fun providePicassoInstance ():Picasso {
         return Picasso.get()
     }
+
+    //Provide an instance of Moshi
+    @Singleton //This indicates that it is tied to the livecycleScope
+     @Provides
+     fun provideMoshiInstance() :Moshi{
+         return Moshi.Builder()
+                 .add(KotlinJsonAdapterFactory())
+                 .build()
+     }
+
+    //Provide instance of LoggingInterceptor
+    @Singleton
+    @Provides
+    fun provideHttpLogginInterceptor():HttpLoggingInterceptor{
+        return HttpLoggingInterceptor().also {
+                 it.level =HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    //Provide instance of okhttpclient
+    @Singleton
+    @Provides
+    fun provideOkHttpClient( htttpInterceptor:HttpLoggingInterceptor):OkHttpClient{
+           return OkHttpClient.Builder()
+                   .addInterceptor(htttpInterceptor)
+                   .build()
+
+    }
+
+    //Provide an instance of Retrofit
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(moshi:Moshi,okHttpClient: OkHttpClient): Retrofit {
+
+        return Retrofit.Builder()
+                .baseUrl(Constants.CRYPTOCOMPARE_BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .client(okHttpClient)
+                .build()
+
+    }
+
+
+
+
 }
