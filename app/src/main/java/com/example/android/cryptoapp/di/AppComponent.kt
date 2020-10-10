b2ee57c782b43457e66502d2484dc5e2c5046a29
FAVOUR <truelyfavoured@gmail.com>
2020-10-10 15:32:57 +0100

    Scoped app AppComponent.kt to singleton

--- app/src/main/java/com/example/android/cryptoapp/di/AppComponent.kt
+++ app/src/main/java/com/example/android/cryptoapp/di/AppComponent.kt
@@ -1,12 +1,22 @@
 package com.example.android.cryptoapp.di
 
+import android.content.Context
+import com.example.android.cryptoapp.activities.MainActivity
+import com.example.android.cryptoapp.fragments.ConversionFragment
+import dagger.BindsInstance
 import dagger.Component
-import dagger.Module
+import javax.inject.Singleton
 
-
+@Singleton
 @Component(modules = [AppModule::class])
-class AppComponent {
+interface AppComponent {
 
+     @Component.Factory
+    interface  Factory {
+         fun create(@BindsInstance context: Context):AppComponent
+    }
+
+     fun create(conversionFragment: ConversionFragment)
 
 }
 
