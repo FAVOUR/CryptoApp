c6ced7b1900274f925242609568198a7647e640b
FAVOUR <truelyfavoured@gmail.com>
2020-10-10 15:35:25 +0100

    Instantiate the DaggerAppComponent to be call ed as a lazy function

--- app/src/main/java/com/example/android/cryptoapp/App.kt
+++ app/src/main/java/com/example/android/cryptoapp/App.kt
@@ -1,12 +1,21 @@
 package com.example.android.cryptoapp
 
 import android.app.Application
+import com.example.android.cryptoapp.di.DaggerAppComponent
 import timber.log.Timber
 
 class App :Application() {
 
+
+    val appComponent by lazy {
+        DaggerAppComponent.factory().create(this)
+    }
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+
     }
+
+
 }
