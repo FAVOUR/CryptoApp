953b0e2e030480ef9dc0335eac7f5c2f9cface26
FAVOUR <truelyfavoured@gmail.com>
2020-10-28 23:41:58 +0100

    Created the generic viewmodel observer  to make the viewmodel observe till foe a set time  data is available

--- app/src/test/java/com/example/android/cryptoapp/util/LiveDataUtil.kt
+++ app/src/test/java/com/example/android/cryptoapp/util/LiveDataUtil.kt
@@ -1,4 +1,35 @@
 package com.example.android.cryptoapp.util
 
-class LiveDataUtil {
+import android.util.TimeUtils
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+//class LiveDataUtil {
+//}
+
+fun <T> LiveData<T>.getOrAwaitNextValue(
+        time:Long =2 ,
+       timeUnit: TimeUnit=TimeUnit.SECONDS,
+        code:()->Unit):T{
+
+        val latch = CountDownLatch(1)
+         var data_:T? =null
+        val observer = object : Observer<T> {
+                override fun onChanged(t: T) {
+                        latch.countDown()
+                        data_=t
+                        this@getOrAwaitNextValue.removeObserver(this)                }
+
+        }
+
+        this.observeForever(observer)
+
+        if(!latch.await(time,timeUnit)){
+                code.invoke()
+                this.removeObserver(observer)
+        }
+
+        return data_ as T
 }
