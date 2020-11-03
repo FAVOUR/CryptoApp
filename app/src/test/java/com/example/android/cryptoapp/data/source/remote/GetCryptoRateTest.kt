ba528b68cedc6ac43b18bb807fb9b060316ba548
FAVOUR <truelyfavoured@gmail.com>
2020-11-03 23:18:28 +0100

    Initial setup for For mocking Web request

---
+++ app/src/test/java/com/example/android/cryptoapp/data/source/remote/GetCryptoRateTest.kt
@@ -0,0 +1,32 @@
+package com.example.android.cryptoapp.data.source.remote
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class GetCryptoRateTest {
+  lateinit var retrofit: Retrofit
+    @Before
+    fun getClient() {
+        val moshi =Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+        retrofit =Retrofit.Builder()
+                 .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+
+
+
+}
