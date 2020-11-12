eefb7a143c0de75ba5fa584a649f51d1d3464fd7
FAVOUR <truelyfavoured@gmail.com>
2020-11-12 02:49:18 +0100

    Successfully setup the mock servber but yet to start asserting the inputs

--- app/src/test/java/com/example/android/cryptoapp/data/source/remote/GetCryptoRateTest.kt
+++ app/src/test/java/com/example/android/cryptoapp/data/source/remote/GetCryptoRateTest.kt
@@ -4,8 +4,10 @@ import androidx.test.core.app.ApplicationProvider
 import com.example.android.cryptoapp.util.MockResponseFileReader
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -17,34 +19,49 @@ import java.io.InputStream
 
 class GetCryptoRateTest {
   lateinit var retrofit: Retrofit
+
+     var mockServer = MockWebServer()
     @Before
     fun getClient() {
-        val moshi =Moshi.Builder()
-                .add(KotlinJsonAdapterFactory())
-                .build()
-        retrofit =Retrofit.Builder()
-                 .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .build()
+
+        mockServer.start(8080)
+
+//        val moshi =Moshi.Builder()
+//                .add(KotlinJsonAdapterFactory())
+//                .build()
+//        retrofit =Retrofit.Builder()
+//                 .addConverterFactory(MoshiConverterFactory.create(moshi))
+//                .build()
+//
+//
     }
 
 
 
     @After
     fun tearDown() {
+        mockServer.shutdown()
     }
 
 
 
     @Test
     fun checkMockSever (){
-        var mockServer =MockWebServer()
+
 
 //         mockServer.
     }
 
-
+    @Test
     fun makeANetworkRequestAndCheckResult(){
-        val mockServer = MockWebServer().enqueue(MockResponse().setBody(MockResponseFileReader("rates.json").content))
+//        mockServer.enqueue(MockResponse().setBody(MockResponseFileReader("rates.json").content))
+        mockServer.dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+
+                 return  MockResponse().setStatus("200").setBody(MockResponseFileReader("rates.json").content)
+
+            }
+        }
 
     }
 
