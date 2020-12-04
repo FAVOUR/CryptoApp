676faf7590348b384f67c81ed5f09ca14bcf7ed8
FAVOUR <truelyfavoured@gmail.com>
2020-12-04 23:04:42 +0100

    Carried out more cleanups

--- app/src/main/java/com/example/android/cryptoapp/fragments/ConversionFragment.kt
+++ app/src/main/java/com/example/android/cryptoapp/fragments/ConversionFragment.kt
@@ -33,10 +33,7 @@ import java.text.DecimalFormat
 import javax.inject.Inject
 import kotlin.properties.Delegates
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
