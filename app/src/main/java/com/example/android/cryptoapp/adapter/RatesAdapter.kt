5376e033d1fdcc29b6b67cb736966f30259ed5e8
FAVOUR <truelyfavoured@gmail.com>
2021-02-07 00:15:07 +0100

    Fully converted the RatesAdapter.kt to binding at the layout level

--- app/src/main/java/com/example/android/cryptoapp/adapter/RatesAdapter.kt
+++ app/src/main/java/com/example/android/cryptoapp/adapter/RatesAdapter.kt
@@ -13,6 +13,7 @@ import com.example.android.cryptoapp.R
 import com.example.android.cryptoapp.domain.model.CryptoCurrencyRates
 import com.example.android.cryptoapp.adapter.RatesAdapter.RatesViewHolder
 import com.example.android.cryptoapp.databinding.RatesRowItemsBinding
+import com.example.android.cryptoapp.util.Helpers.formatAndReturnString
 import com.google.gson.Gson
 import java.text.DecimalFormat
 
@@ -20,13 +21,6 @@ import java.text.DecimalFormat
 //class RatesAdapter(private val mContext: Context, private val mResults: MutableList<Results>, private val mOnClickedListiner: ListItemClickListiner) : RecyclerView.Adapter<Rates_ViewHoler>() {
 class RatesAdapter(private val mResults: MutableList<CryptoCurrencyRates>, private  val mListItemClickListiner :ListItemClickListiner) : RecyclerView.Adapter<RatesViewHolder>() {
     private var clickedPosition = 0
-    private lateinit var currencyImage: ImageView
-    private lateinit var currencyAbr: TextView
-    private lateinit var exchangeRate_1: TextView
-    private lateinit var currencyName: TextView
-    private lateinit var exchangeRate_2: TextView
-    private lateinit var currencySymbol_1: TextView
-    private lateinit var currencySymbol_2: TextView
     private lateinit var mOnClickedListiner: ListItemClickListiner
 
 
@@ -37,6 +31,7 @@ class RatesAdapter(private val mResults: MutableList<CryptoCurrencyRates>, priva
         val layoutInflater = LayoutInflater.from(getCurrentContext)
         val newViewHolder:RatesRowItemsBinding  = DataBindingUtil.inflate(layoutInflater,R.layout.rates_row_items, parent, false)
         return RatesViewHolder(newViewHolder)
+
     }
 
     override fun onBindViewHolder(holder: RatesViewHolder, position: Int) {
@@ -63,41 +58,41 @@ class RatesAdapter(private val mResults: MutableList<CryptoCurrencyRates>, priva
 
     inner class RatesViewHolder(val binding: RatesRowItemsBinding) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
         fun bind(listIndex: Int) {
-            var format: DecimalFormat? = null
-            format = DecimalFormat()
-            format.isGroupingUsed = true
-            format.maximumIntegerDigits = 10
-            format.maximumFractionDigits = 3
 
-            binding.btcResult.text=(format.format(mResults[listIndex].firstExRate))
-            binding.currencySymbol.text=(mResults[listIndex].symbol)
-            binding.ethResult.text=(format.format(mResults[listIndex].secondExRate))
-            binding.currencySymbol1.text=(mResults[listIndex].symbol)
-            binding.currencyAbr.text=(mResults[listIndex].abbrivation)
-            binding.currencyImage?.setImageResource(mResults[listIndex].image)
-            binding.currencyName.text=(mResults[listIndex].name)
 
+            binding.cryptoData=mResults[listIndex]
+
+//            binding.btcResult.text=(format.format(mResults[listIndex].firstExRate))
+//            binding.currencySymbol.text=(mResults[listIndex].symbol)
+//            binding.ethResult.text=(formatAndReturnString((mResults[listIndex].secondExRate)))
+//            binding.ethResult.text=((mResults[listIndex].secondExRate).formatAndReturnString())
+//            binding.currencySymbol1.text=(mResults[listIndex].symbol)
+//            binding.currencyAbr.text=(mResults[listIndex].abbrivation)
+//            binding.currencyImage.setImageResource(mResults[listIndex].image)
+//            binding.currencyName.text=(mResults[listIndex].name)
+           binding.executePendingBindings()
         }
 
         override fun onClick(view: View) {
 
-//             Set the body of the function to get the position which is the item that was clicked
+//         Set the body of the function to get the position which is the item that was clicked
             clickedPosition = adapterPosition
 
 
-//             This invokes the onclick listener of the other class by passing clickedPosition value
+//        This invokes the onclick listener of the other class by passing clickedPosition value
             mOnClickedListiner.onListItemClicked(mResults[clickedPosition])
         }
 
         init {
-            Log.e("Init",  "Here ")
+
+            var format: DecimalFormat = DecimalFormat()
+            format.isGroupingUsed = true
+            format.maximumIntegerDigits = 10
+            format.maximumFractionDigits = 3
 
             itemView.setOnClickListener(this)
 
              mOnClickedListiner =mListItemClickListiner
-
-
-
         }
     }
 
@@ -107,7 +102,7 @@ class RatesAdapter(private val mResults: MutableList<CryptoCurrencyRates>, priva
         mResults.addAll(results)
         Log.e("results",Gson().toJson(results))
         notifyDataSetChanged()
-//        notifyItemChanged(mResults.size)
+//      notifyItemChanged(mResults.size)
 
     }
 
