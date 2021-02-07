194a9c43bfa48c8a614a9425acec5aa5b9bc4be7
FAVOUR <truelyfavoured@gmail.com>
2021-02-07 20:14:48 +0100

    Updated the CryptoCurrencyRates.kt class by adding an Id of type long

--- app/src/main/java/com/example/android/cryptoapp/domain/model/CryptoCurrencyRates.kt
+++ app/src/main/java/com/example/android/cryptoapp/domain/model/CryptoCurrencyRates.kt
@@ -6,14 +6,14 @@ import com.example.android.cryptoapp.data.source.local.db.CryptoCurrencyData
 //TODO
  /*  1)change the parameter name of first and second rate
    2) Rename the class probably currency crypto */
-data class CryptoCurrencyRates(val image: Int, val firstExRate: Double, val secondExRate: Double, val name: String, val abbrivation: String, val symbol: String){
+data class CryptoCurrencyRates(var  id:Long, val image: Int, val firstExRate: Double, val secondExRate: Double, val name: String, val abbrivation: String, val symbol: String){
 
 
 }
-    fun  List<CryptoCurrencyData>.asDomainModel():List<CryptoCurrencyRates>{
+    fun  List<CryptoCurrencyData>.asUIModel():List<CryptoCurrencyRates>{
 
         return map {
-            CryptoCurrencyRates(image = it.image,firstExRate =  it.btcRate,secondExRate =  it.ethRate, name = it.currencyName, abbrivation = it.currencyAbbreviation, symbol = it.currencySymbol)
+            CryptoCurrencyRates(id = it.id,image = it.image,firstExRate =  it.btcRate,secondExRate =  it.ethRate, name = it.currencyName, abbrivation = it.currencyAbbreviation, symbol = it.currencySymbol)
         }
 
     }
