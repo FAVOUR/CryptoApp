66eee38ec1cfd55f7ee82b1b0044606d6abc8300
FAVOUR <truelyfavoured@gmail.com>
2021-02-07 19:59:04 +0100

    Setup a class to handle the Listiners for ListFragments

---
+++ app/src/main/java/com/example/android/cryptoapp/util/Listeners.kt
@@ -0,0 +1,17 @@
+package com.example.android.cryptoapp.util
+
+import com.example.android.cryptoapp.domain.model.CryptoCurrencyRates
+
+class Listeners {
+
+    interface ListItemClickListiner {
+        fun onListItemClicked(result: CryptoCurrencyRates)
+    }
+
+
+    interface LongItemClickedListener{
+        fun onLongItemClickedListener(itemId:Long){
+
+        }
+    }
+}
