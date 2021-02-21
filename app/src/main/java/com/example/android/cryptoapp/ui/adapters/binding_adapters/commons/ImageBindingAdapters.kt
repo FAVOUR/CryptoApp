7d1e731c1f648a34d2e652732668d60d11a78e97
FAVOUR <truelyfavoured@gmail.com>
2021-02-21 20:20:39 +0100

    Did some cleanup in the image binding AdapterComponent

--- app/src/main/java/com/example/android/cryptoapp/ui/adapters/binding_adapters/commons/ImageBindingAdapters.kt
+++ app/src/main/java/com/example/android/cryptoapp/ui/adapters/binding_adapters/commons/ImageBindingAdapters.kt
@@ -3,20 +3,21 @@ package com.example.android.cryptoapp.ui.adapters.binding_adapters.commons
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.example.android.cryptoapp.R
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import javax.inject.Inject
 
 
- class ImageAdapters @Inject constructor(val picasso: Picasso) {
-
+ class ImageBindingAdapters @Inject constructor(val picasso: Picasso) {
 
          @BindingAdapter("icon")
-         fun ImageView.setImage(imageResId: String) {
+         fun ImageView.setImage(imageResId: Int) {
              picasso.load(imageResId)
                      .transform(CropCircleTransformation())
                      .into(this)
          }
+
 
 
   }
