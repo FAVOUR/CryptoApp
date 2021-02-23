552b313a161202281097e13db51cf5e75bf53256
FAVOUR <truelyfavoured@gmail.com>
2021-02-23 23:02:08 +0100

    Fixed the an issue with the DB test case by making the id 1 instead of 0

--- app/src/sharedTest/java/com.example.android.cryptoapp/TestData.kt
+++ app/src/sharedTest/java/com.example.android.cryptoapp/TestData.kt
@@ -5,7 +5,7 @@ import com.example.android.cryptoapp.data.source.local.db.CryptoCurrencyData
 object TestData {
 
      fun getCryptoCurrencyData() : CryptoCurrencyData {
-         return CryptoCurrencyData(currencyName = "Nigeria", currencyAbbreviation = "NGN",currencySymbol = "",image =12345,btcRate = 3.65,ethRate = 234.56)
+         return CryptoCurrencyData(id=1,currencyName = "Nigeria", currencyAbbreviation = "NGN",currencySymbol = "",image =12345,btcRate = 3.65,ethRate = 234.56)
      }
 
 }
