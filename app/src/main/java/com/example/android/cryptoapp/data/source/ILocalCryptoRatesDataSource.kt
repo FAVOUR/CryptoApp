172a7b2c0c6e7d7fc264677e2cbfca8f49cbe861
FAVOUR <truelyfavoured@gmail.com>
2021-02-28 23:03:14 +0100

    Did some spacing

--- app/src/main/java/com/example/android/cryptoapp/data/source/ILocalCryptoRatesDataSource.kt
+++ app/src/main/java/com/example/android/cryptoapp/data/source/ILocalCryptoRatesDataSource.kt
@@ -1,10 +1,7 @@
 package com.example.android.cryptoapp.data.source
 
 import androidx.lifecycle.LiveData
-import com.example.android.cryptoapp.currency_data.CurrencyAbbreviation
-import com.example.android.cryptoapp.data.source.CommonCryptoRatesDataSource
 import com.example.android.cryptoapp.data.source.local.db.CryptoCurrencyData
-import com.example.android.cryptoapp.domain.model.CryptoCurrencyRates
 import com.example.android.cryptoapp.util.Result
 
 interface ILocalCryptoRatesDataSource : CommonCryptoRatesDataSource {
@@ -12,6 +9,7 @@ interface ILocalCryptoRatesDataSource : CommonCryptoRatesDataSource {
     fun observeCryptoRates(): LiveData<Result<List<CryptoCurrencyData>>>
 
     suspend fun getCryptoRateById(id: Int): Result<CryptoCurrencyData>
+
     suspend fun getCryptoRate(): Result<List<CryptoCurrencyData>>
 
     suspend fun saveCryptoRates(cryptoRateData : List<CryptoCurrencyData>)
